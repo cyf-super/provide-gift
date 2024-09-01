40a5448ee58f5b153ccbd15a14fa9646c3715800
CYFHero <1753760980@qq.com>
2024-09-01 22:55:41 +0800

    优化图片

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,12 @@ function App() {
   }, [isOpen]);
 
   const toggleDrawer = () => {
-    setIsOpen(prevState => !prevState);
+    const newState = !isOpen;
+    setIsOpen(newState);
+    if (!newState) {
+      setItem({} as Item);
+      setType('');
+    }
   };
 
   const onView = (type: string, item: Item) => {
@@ -27,15 +32,36 @@ function App() {
     toggleDrawer();
   };
 
+  const onCopy = (type: string, name: string) => {
+    copyToClipboard(`【${type}】— ${name}`);
+  };
+
+  const copyToClipboard = (text: string) => {
+    // 创建一个临时的 input 元素
+    const tempInput = document.createElement('input');
+    // 设置其值为要复制的文本
+    tempInput.value = text;
+    // 将临时 input 添加到文档中
+    document.body.appendChild(tempInput);
+    // 选中 input 中的文本
+    tempInput.select();
+    // 执行复制命令
+    document.execCommand('copy');
+    // 移除临时 input
+    document.body.removeChild(tempInput);
+  };
+
   return (
     <>
       <div className={styles.box}>
-        <div className="shopTitle">长安不只三万里</div>
+        <div className="shopTitle">长安不止三万里</div>
         <header className="header">
           <h2>好评送礼</h2>
           <div className="detail">
-            <span>几个字</span> + <span className="start">五星好评</span>
-            您可以任选一份资料【免费赠送】
+            <div className="first">
+              <span>几个字</span> + <span className="start">五星好评</span>
+            </div>
+            <div>您可以任选一份资料【免费赠送】</div>
           </div>
           <p>非常感谢您</p>
         </header>
@@ -68,7 +94,12 @@ function App() {
                     <div className="name">{data.name}</div>
                     <div className="handle">
                       <span onClick={() => onView(item.type, data)}>详情</span>
-                      <span className="copy">复制</span>
+                      <span
+                        className="copy"
+                        onClick={() => onCopy(item.type, data.name)}
+                      >
+                        复制
+                      </span>
                     </div>
                   </li>
                 ))}
