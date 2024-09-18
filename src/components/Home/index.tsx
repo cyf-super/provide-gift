c3a1c180b0e485ddef23be0a431683df22d6369d
CYF <caiyuefeng@xmiles.cn>
2024-09-18 17:29:50 +0800

    添加资源

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,7 +1,6 @@
 import styles from './index.module.scss';
 import daily from '../../data/daily.json';
 import { motion } from 'framer-motion';
-import { Link } from 'react-router-dom';
 
 const visible = { opacity: 1, y: 0, transition: { duration: 0.5 } };
 
@@ -37,7 +36,7 @@ export default function Home() {
             </div>
             <div>您可以任选一份资料【免费赠送】</div>
           </div>
-          <p>越来越好</p>
+          <p>希望能够对您有帮助～</p>
         </motion.header>
         <motion.section className="yijvBox" variants={itemVariants}>
           <div className="yijv">每日一句</div>
@@ -45,6 +44,9 @@ export default function Home() {
             dangerouslySetInnerHTML={{ __html: daily[new Date().getDate()] }}
             className="detail"
           ></div>
+        </motion.section>
+        <motion.section className="explain">
+          保持更新，每周都会添加新资源~
         </motion.section>
       </motion.div>
     </>
